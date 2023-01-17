6af5d1f630098f6946c316c5cc1e2273028a86c3
Mason Hall <masonhall@gmail.com>
2023-01-17 14:41:47 -0500

    using VCR.py (no network requests)

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,3 +12,8 @@ def fcc() -> MerkleApiClient:
     access_token = os.getenv("AUTH")
     assert access_token, "AUTH env var not set"
     return MerkleApiClient(access_token=access_token)
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    return {"filter_headers": ["authorization", "DUMMY"]}
