9a0a8b9aaeeef0bd758fd9cdd3dcd2e461598a93
alex paden <padenalex0@gmail.com>
2023-03-18 20:06:13 -0500

    Update ApiCastReaction
    change cast_hash Field regex

--- farcaster/models.py
+++ farcaster/models.py
@@ -177,7 +177,7 @@ class ApiCastReaction(BaseModel):
     hash: str
     reactor: ApiUser
     timestamp: PositiveInt
-    cast_hash: str = Field(..., regex=r"^0[xX][0-9a-fA-F]{64}|[0-9a-fA-F]{40}$")
+    cast_hash: str = Field(..., regex=r"^0x[0-9a-fA-F]{40}$")
 
 
 class ApiNewCollection(BaseModel):
